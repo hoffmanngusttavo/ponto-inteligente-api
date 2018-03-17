b8b470782d8c62f07ed8f22cd37fa8db0c30b72d
Hoffmann <hoffmann.gusttavo@gmail.com>
2018-03-17 11:06:33 -0300

    teste

--- src/main/java/com/hoffmann/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/hoffmann/pontointeligente/api/PontoInteligenteApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PontoInteligenteApplication {
 
 	public static void main(String[] args) {
+					
+		System.out.println("alguma coisa");
+		
 		SpringApplication.run(PontoInteligenteApplication.class, args);
 	}
 }
